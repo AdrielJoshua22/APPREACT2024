f24c0633f65bd7b2c35133447e6d5424b539e521
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-04-28 20:54:47 -0300

    mFetch

--- MiProyecto/src/components/ItemListContainer/ItemListContainer.jsx
+++ MiProyecto/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { mFetch } from "../../utils/mFetch"
 
-export const ItemListContainer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+
+ export const ItemListContainer = ({greeting}) => {
+    const [productos, setProductos] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(()=>{
+        mFetch()
+        // .then(res => res.json())
+        .then( resultado=> { 
+            setProductos(resultado)
+        })
+        // .then(resul => console.log(resul))
+        .catch( error => console.log(error) )
+        .finally(()=> setIsLoading(false))
+    }, [])
+
+    console.log(productos)
+
+    //map <- nuevo array transformadio mediante una función transformadora
+    // [1,2,3,4,5,6,7] <- map <- [<li key=1 >1</li>, <li key=2 >2</li>, <li key=3 >3</li>, ....]
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: 'row',
+            flexWrap: "wrap"
+        }}>
+            
+            { isLoading ?
+                    <h2>Cargando...</h2>
+                : 
+                    productos.map( ({id, foto, name, price, categoria}) =>  <div key={id} className="card w-25">
+                                                        <img src={foto} className="card-img-top" alt="imagen-card" />
+                                                        <div className="card-body">
+                                                            <h6>Nombre: {name}</h6>
+                                                            <label>Precio: {price}</label>
+                                                            <label>Categoria: {categoria}</label>
+                                                        </div>
+                                                        <div className="card-footer">
+                                                            <button className="btn btn-outline-dark">Detalle</button>
+                                                        </div>
+                                                    </div>
+                    ) 
+            }
+            
+        </div>
+    )
 }
 
