470bb3e7d72c690478f7bcda5c9b4f202b674cf7
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-02 10:09:34 -0300

    mejoro los stylos

--- MiProyecto/src/utils/mFetch.js
+++ MiProyecto/src/utils/mFetch.js
@@ -1,11 +1,8 @@
 let productos = [
-    { id: '1' ,categoria: 'remeras', name: "producto 1", stock:50, price: 37, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kPXPtaX9/200/200/original?country=ar', colores:[{nombre:'rojo', clase:''}] },
-    { id: '2' ,categoria: 'remeras', name: "producto 2", stock:50, price: 27, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqX3vl0y/200/200/original?country=ar' },
-    { id: '3' ,categoria: 'remeras', name: "producto 3", stock:50, price: 40, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqX093YV/200/200/original?country=ar' },
-    { id: '4' ,categoria: 'gorras',  name: "producto 4", stock:50, price: 55, foto:''},
-    { id: '6' ,categoria: 'gorras',  name: "producto 6", stock:50, price: 19, foto:''},
-    { id: '7' ,categoria: 'gorras',  name: "producto 7", stock:50, price: 2,  foto:''},
-] 
+    { id: '1' ,categoria: 'Hamburguesas', name: "producto 1", stock:50, price: 37, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kPXPtaX9/200/200/original?country=ar', colores:[{nombre:'rojo', clase:''}] },
+    { id: '2' ,categoria: 'Hamburguesas', name: "producto 2", stock:50, price: 27, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqX3vl0y/200/200/original?country=ar' },
+    { id: '3' ,categoria: 'Hamburguesas', name: "producto 3", stock:50, price: 40, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqX093YV/200/200/original?country=ar' },
+]
 
 export const mFetch = () =>{
     return new Promise((res, rej)=>{
