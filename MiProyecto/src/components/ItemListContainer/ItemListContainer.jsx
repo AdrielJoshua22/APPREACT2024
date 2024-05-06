0051d5d150499af1fb596bdc09f65a352949c85e
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-05 21:26:16 -0300

    ItemListContainer

--- MiProyecto/src/components/ItemListContainer/ItemListContainer.jsx
+++ MiProyecto/src/components/ItemListContainer/ItemListContainer.jsx
@@ -24,7 +24,8 @@ import './ItemListContainer.css'
     //map <- nuevo array transformadio mediante una función transformadora
     // [1,2,3,4,5,6,7] <- map <- [<li key=1 >1</li>, <li key=2 >2</li>, <li key=3 >3</li>, ....]
     return (
-        <div style={{
+        <div className="card m-3"
+       style={{
             display: "flex",
             flexDirection: 'row',
             flexWrap: "wrap"
--- MiProyecto/src/utils/mFetch.js
+++ MiProyecto/src/utils/mFetch.js
@@ -1,7 +1,8 @@
 let productos = [
     { id: '1' ,categoria: 'Hamburguesas', name: "producto 1", stock:50, price: 37, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kPXPtaX9/200/200/original?country=ar', colores:[{nombre:'rojo', clase:''}] },
     { id: '2' ,categoria: 'Hamburguesas', name: "producto 2", stock:50, price: 27, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqX3vl0y/200/200/original?country=ar' },
-    { id: '3' ,categoria: 'Hamburguesas', name: "producto 3", stock:50, price: 40, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqX093YV/200/200/original?country=ar' },
+    { id: '1' ,categoria: 'Hamburguesas', name: "producto 1", stock:50, price: 37, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kPXPtaX9/200/200/original?country=ar', colores:[{nombre:'rojo', clase:''}] },
+    { id: '4' ,categoria: 'Hamburguesas', name: "producto 3", stock:50, price: 40, foto:'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kqX093YV/200/200/original?country=ar' },
 ]
 
 export const mFetch = () =>{
