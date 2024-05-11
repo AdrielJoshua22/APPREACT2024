adc6a76e6fd45b38306ae12bbb99d9e52e252d38
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-11 17:26:02 -0300

    ok spinner

--- MiProyecto/src/components/ItemListContainer/ItemListContainer.jsx
+++ MiProyecto/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react"
- import { mFetch } from "../../utils/mFetch" 
-import './ItemListContainer.css'
-import { Contador } from "../contador/contador"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { mFetch } from "../../utils/mFetch";
+import './ItemListContainer.css';
+import { Contador } from "../contador/contador";
+import { Link } from "react-router-dom";
 import Spinner from '../Spinner/Spinner';
 
- export const ItemListContainer = ({greeting}) => {
-    const [productos, setProductos] = useState([])
-    const [loading, setLoading] = useState(true)
+export const ItemListContainer = ({greeting}) => {
+    const [productos, setProductos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        mFetch('https://api.example.com/data')
+        mFetch()
           .then(response => response.json())
           .then(productos => {
             setProductos(productos);
@@ -18,33 +18,27 @@ import Spinner from '../Spinner/Spinner';
           });
       }, []);
 
-      if (loading) {
-        return <Spinner />;
-      }
-
-    console.log(productos)
+    console.log(productos);
     return (
         <div className="card-container">
-            { <Spinner/> ?
-                <h2>Cargando...</h2>
-                : 
+            {loading ? <Spinner /> : 
                 productos.map( ({id, foto, name, price, categoria}) =>  
                     <div key={id} className="card">
                         <Link to={`/detail/${id}`}>
-                        <img src={foto} className="card-img-top" alt="imagen-card" />
-                        <div className="card-body">
-                            <h6 className="nombre">Nombre: {name}</h6>
-                            <label className="precio">Precio: {price}</label>
-                            <label className="categoria">Categoria: {categoria}</label>
-                        </div>
-                        <div className="card-footer">
-                            <button className="detalle">Detalle</button>
-                            <Contador/>
-                        </div>
+                            <img src={foto} className="card-img-top" alt="imagen-card" />
+                            <div className="card-body">
+                                <h6 className="nombre">Nombre: {name}</h6>
+                                <label className="precio">Precio: {price}</label>
+                                <label className="categoria">Categoria: {categoria}</label>
+                            </div>
+                            <div className="card-footer">
+                                <button className="detalle">Detalle</button>
+                                <Contador/>
+                            </div>
                         </Link>
                     </div>
-                ) 
+                )
             }
         </div>
-    )
+    );
 }
