d9de51f2809822c942dc2627cd171a224752b9f8
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-11 12:59:09 -0300

    creo componente spinner.

---
+++ MiProyecto/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.css'; // Asegúrate de tener un archivo CSS llamado Spinner.css en la misma carpeta
+
+const Spinner = () => {
+  return (
+    <div className="spinner"></div>
+  );
+};
+
+export default Spinner;
