b1742475e6aed9cf2bb1a7af1324e995563dde1b
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-17 11:28:58 -0300

    ok

--- MiProyecto/src/components/ItemListContainer/ItemListContainer.jsx
+++ MiProyecto/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,43 +1,41 @@
 import { useEffect, useState } from "react";
 import { mFetch } from "../../utils/mFetch";
 import './ItemListContainer.css';
-import { Contador } from "../contador/contador";
 import { Link } from "react-router-dom";
 import Spinner from '../Spinner/Spinner';
 
-export const ItemListContainer = ({greeting}) => {
+export const ItemListContainer = ({ greeting }) => {
     const [productos, setProductos] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         setLoading(true);
         mFetch()
-          .then(productos => {
-            setTimeout(() => {
-              setProductos(productos);
-              setLoading(false);
-            }, 1000); // Espera 1 segundo antes de mostrar los productos
-          });
-      }, []);
+            .then(productos => {
+                setTimeout(() => {
+                    setProductos(productos);
+                    setLoading(false);
+                }, 1000);
+            });
+    }, []);
 
     console.log(productos);
     return (
         <div className="card-container">
-            {loading ? <Spinner/> : 
-                productos.map( ({id, foto, name, price, categoria}) =>  
+            {loading ? <Spinner /> :
+                productos.map(({ id, foto, name, price, categoria }) =>
                     <div key={id} className="card">
-                            <img src={foto} className="card-img-top" alt="imagen-card" />
-                            <div className="card-body">
-                                <h6 className="nombre">Nombre: {name}</h6>
-                                <label className="precio">Precio: {price}</label>
-                                <label className="categoria">Categoria: {categoria}</label>
-                            </div>
-                            <div className="card-footer">
-                        <Link to={`/detail/${id}`}>
+                        <img src={foto} className="card-img-top" alt="imagen-card" />
+                        <div className="card-body">
+                            <h6 className="nombre">Nombre: {name}</h6>
+                            <label className="precio">Precio: {price}</label>
+                            <label className="categoria">Categoria: {categoria}</label>
+                        </div>
+                        <div className="card-footer">
+                            <Link to={`/detail/${id}`}>
                                 <button className="detalle">Detalle</button>
-                        </Link>
-                             {/*    <Contador/> */}
-                            </div>
+                            </Link>
+                        </div>
                     </div>
                 )
             }
