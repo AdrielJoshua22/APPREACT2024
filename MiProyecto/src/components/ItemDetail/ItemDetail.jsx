6fd1d6b9a94ac25a33c019aa382dec6bb41d2baf
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-19 20:33:18 -0300

    mejoro stylos

--- MiProyecto/src/components/ItemDetail/ItemDetail.jsx
+++ MiProyecto/src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './ItemDetail.css';
+
 export const ItemDetail = ({ title, imageUrl, price, description }) => {
   return (
-    <div className="item-detail">
-      <h2>{title}</h2>
-      <img src={imageUrl} alt={title} />
-      <p className="price">Precio: ${price}</p>
-      <p>{description}</p>
+    <div className="item-detail-container">
+      <div className="item-image">
+        <img src={imageUrl} alt={title} />
+      </div>
+      <div className="item-info">
+        <h2>{title}</h2>
+        <p className="price">Precio: ${price}</p>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
