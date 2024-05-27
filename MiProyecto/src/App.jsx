2962f82839c2c9a6ef9e55af1ec54c0ec789ca8a
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-27 09:49:54 -0300

    Uso Carousel de reactboostrap. no funciona

--- MiProyecto/src/App.jsx
+++ MiProyecto/src/App.jsx
@@ -1,26 +1,26 @@
-import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
-import './App.css'
-import { Footer } from './components/Footer/Footer'
-import NavBar from './components/Menu/NavBar'
-import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
-import { CartContainer } from './components/CartContainer/CartContainer'
-import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer'
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Footer } from './components/Footer/Footer';
+import NavBar from './components/Menu/NavBar';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import { CartContainer } from './components/CartContainer/CartContainer';
+import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
+import { ControlledCarousel } from './components/Carousel/Carousel';
 
 function App() {
-
   return (
     <Router>
       <NavBar />
-        <Routes>
-        <Route path='/' element={<ItemListContainer gretting={'Hola soy item list container'}/>}/>
-        <Route path='/detail/:pid' element={<ItemDetailContainer/>}/>
-        <Route path='/cart' element={<CartContainer/>}/>
-        </Routes>
+      <ControlledCarousel/>
+      <Routes>
+        <Route path='/' element={<ItemListContainer greeting={'Hola soy item list container'} />} />
+        <Route path='/detail/:pid' element={<ItemDetailContainer />} />
+        <Route path='/cart' element={<CartContainer />} />
+      </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
+
