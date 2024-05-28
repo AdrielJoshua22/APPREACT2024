4492c4ec4844aece934aa75a4753fe4e9bbeba73
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-28 12:39:13 -0300

    ok

--- MiProyecto/src/components/Carousel/Carousel.jsx
+++ MiProyecto/src/components/Carousel/Carousel.jsx
@@ -1,37 +1,45 @@
-import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-/* import ExampleCarouselImage from 'components/ExampleCarouselImage'; */
 
-export function ControlledCarousel() {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
-
+function DarkVariantExample() {
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
+    <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=f5f5f5"
+          alt="First slide"
+        />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=eee"
+          alt="Second slide"
+        />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+          alt="Third slide"
+        />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
 }
+
+export default DarkVariantExample;
