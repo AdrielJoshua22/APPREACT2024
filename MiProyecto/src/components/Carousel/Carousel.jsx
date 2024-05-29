1594abfc96c3c1c2d8712adaa5e48a53ecedba56
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-05-29 12:21:24 -0300

    ok

--- MiProyecto/src/components/Carousel/Carousel.jsx
+++ MiProyecto/src/components/Carousel/Carousel.jsx
@@ -6,9 +6,10 @@ export function DarkVariantExample() {
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
         <img
-          className="d-block w-100 h-75"
+          className="d-block h-75 w-100 h-75"
           src="./Img/img.jpg"
           alt="First slide"
+          style={{ height: '300px' }}
         />
         <Carousel.Caption>
          {/*  <h5>First slide label</h5>
