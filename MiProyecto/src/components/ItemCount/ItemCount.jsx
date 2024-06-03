d51d79f51774fa134fd6570f9601fcaa56ce4d78
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-02 21:23:45 -0300

    setcount

--- MiProyecto/src/components/ItemCount/ItemCount.jsx
+++ MiProyecto/src/components/ItemCount/ItemCount.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export const ItemCount = () => {
+    const [isCant, setIsCant] = useState(false)
+
+    const onAdd=()=>{
+        console.log('la cantidad seleccionada :',cantidad)
+        setIsCant(true)
+    }
   return (
     <div>
       
