9343f96d5e5735a0335943a5bccac7296d3d25c5
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-03 10:02:55 -0300

    ok

--- MiProyecto/src/components/Contador/Contador.jsx
+++ MiProyecto/src/components/Contador/Contador.jsx
@@ -1,8 +1,8 @@
 import { Container } from 'react-bootstrap'
 import { useCounter } from '../../Hooks/useCounter'
 import './contador.css'
-import { Link } from "react-router-dom";
-
+/* import { Link } from "react-router-dom";
+ */
 export const Contador = ({initial=1,stock=5,onAdd}) => {
 
   const {contador,incrementar,decrementar,resetear} = useCounter(initial,1,stock)
