3b4bec0b7d1f4e23cfb3467745df1b4c3fcb3e91
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-06 09:47:21 -0300

    ok

--- MiProyecto/src/components/ItemDetail/ItemDetail.jsx
+++ MiProyecto/src/components/ItemDetail/ItemDetail.jsx
@@ -7,15 +7,15 @@ import {useState} from 'react';
 export const ItemDetail = ({ title, imageUrl, price, description }) => {
   const [isCant, setIsCant] = useState(false)
 
-  const onAdd= (cantidad)=>{
+/*   const onAdd= (cantidad)=>{
     console.log('cantidad seleccionada :',cantidad)
     setIsCant(true)
-  }
+  } */
 
   return (
-    !isCant ?
+   /*  !isCant ?
     <Contador onAdd={onAdd}/>
-    :
+    : */
     <div className="item-detail-container">
       <div className="item-image">
         <img src={imageUrl} alt={title} />
@@ -24,7 +24,7 @@ export const ItemDetail = ({ title, imageUrl, price, description }) => {
         <h2>{title}</h2>
         <p className="price">Precio: ${price}</p>
         <p>{description}</p>
-       /*  <Contador/> */
+       <Contador/> 
       </div>
     </div>
   );
