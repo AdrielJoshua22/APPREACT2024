6c2142934ad7ae2549c93781842d1f931e1d453f
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-08 11:19:54 -0300

    z

--- MiProyecto/src/components/Contador/Contador.jsx
+++ MiProyecto/src/components/Contador/Contador.jsx
@@ -1,8 +1,8 @@
 import { Container } from 'react-bootstrap'
 import { useCounter } from '../../Hooks/useCounter'
 import './contador.css'
-/* import { Link } from "react-router-dom";
- */
+import { Link } from "react-router-dom";
+
 export const Contador = ({initial=1,stock=5,onAdd}) => {
 
   const {contador,incrementar,decrementar,resetear} = useCounter(initial,1,stock)
@@ -13,7 +13,7 @@ export const Contador = ({initial=1,stock=5,onAdd}) => {
     <button className='contador' onClick={() => decrementar()}>-1</button>
     <button  className='contador' onClick={() => resetear()}>Reset</button>
     <button  className='contador' onClick={() => incrementar()}>+1</button>
-  <button  className='contador' onClick={()=>{onAdd(Contador)}} >Agregar a carrito</button>
+  <Link to={`/cart`}className='contador' onClick={()=>{onAdd(Contador)}} >Agregar a carrito</Link>
     </Container>
   ) 
 }
