16c40692018161845c989982b04eda687dab2865
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-09 19:39:43 -0300

    ok

--- MiProyecto/src/components/CartContainer/CartContainer.jsx
+++ MiProyecto/src/components/CartContainer/CartContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export const CartContainer = () => {
+export const CartContainer = ({ cartItems }) => {
   return (
-    <>
-      
-    </>
+    <div>
+      {cartItems.map((item) => (
+        <div key={item.id}>
+          {item.name}
+        </div>
+      ))}
+    </div>
   )
 }
-
-
