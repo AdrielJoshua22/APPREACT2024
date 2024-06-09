5a679ec0b6ad69071af9304960e02727b0a727ca
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-09 19:33:37 -0300

    ok

--- MiProyecto/src/components/CartContainer/CartContainer.jsx
+++ MiProyecto/src/components/CartContainer/CartContainer.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 export const CartContainer = () => {
   return (
-    <div>
-      <h3>🛒🛒🛒🛒🛒🛒
-        ESTAS EN EL CARRITO Y ESTE COMPONENTE 
-        ESTA FUNCIONANDO!</h3>
-    </div>
+    <>
+      
+    </>
   )
 }
 
