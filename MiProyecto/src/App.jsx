d93178cdae20f47b5ad3752a7f879c32385a384e
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-20 19:36:07 -0300

    agrego cartContextProvider

--- MiProyecto/src/App.jsx
+++ MiProyecto/src/App.jsx
@@ -5,33 +5,33 @@ import NavBar from './components/Menu/NavBar';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { CartContainer } from './components/CartContainer/CartContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
-import {  DarkVariantExample } from './components/Carousel/Carousel'; 
-import { createContext } from 'react'; 
-import { CartContext } from './context/CartContext';
+import { DarkVariantExample } from './components/Carousel/Carousel';
+import { createContext } from 'react';
+import { CartContextProvider } from './context/CartContext';
 
 function App() {
-const ContextApp = createContext();
-console.log(ContextApp)
+  const ContextApp = createContext();
+  console.log(ContextApp)
 
   return (
-    <CartContext.Provider value={{
 
-    }}>
-    <Router>
-     
-      <NavBar />
-      <br />
-     <DarkVariantExample/> 
-     <br />
-      <Routes>
-        <Route path='/' element={<ItemListContainer greeting={'Hola soy item list container'} />} />
-        <Route path='/detail/:pid' element={<ItemDetailContainer />} />
-        <Route path='/cart' element={<CartContainer />} />
-      {/*   <Route path='/details' element={<ItemDetaiL/>}/> */}
-      </Routes>
-      <Footer />
-    </Router>
-    </CartContext.Provider>
+    <CartContextProvider>
+      <Router>
+
+        <NavBar />
+        <br />
+        <DarkVariantExample />
+        <br />
+        <Routes>
+          <Route path='/' element={<ItemListContainer greeting={'Hola soy item list container'} />} />
+          <Route path='/detail/:pid' element={<ItemDetailContainer />} />
+          <Route path='/cart' element={<CartContainer />} />
+          {/*   <Route path='/details' element={<ItemDetaiL/>}/> */}
+        </Routes>
+        <Footer />
+      </Router>
+
+    </CartContextProvider>
   );
 }
 
