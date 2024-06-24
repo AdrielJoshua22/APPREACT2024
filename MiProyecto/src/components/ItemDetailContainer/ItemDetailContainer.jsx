f66e7b9b99a9cfda7ecd38d09a9e9aa617cb2980
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-24 20:37:07 -0300

    ahora si funciona!!!

--- MiProyecto/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ MiProyecto/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -8,15 +8,24 @@ export const ItemDetailContainer = () => {
     const [isLoading, setIsLoading] = useState(true)
     const { pid } = useParams() 
 
-    useEffect(()=>{ 
+/*     useEffect(()=>{ 
 
         mFetch(pid)
         .then(resp => setProducto(resp))
         .catch(err => console.log(err))
         .finally(()=> setIsLoading(false))
 
-    }, [])
+    }, []) */
     
+useEffect(()=>{
+    setIsLoading(true);
+    mFetch().then((productos)=>{
+        const prod = productos.find((p)=>p.id === pid);
+        setProducto(prod);
+        setIsLoading(false);
+    })
+},[pid])
+
 
     console.log(pid)
     return (
