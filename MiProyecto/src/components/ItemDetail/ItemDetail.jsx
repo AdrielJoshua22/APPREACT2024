9cbb85cfd96211168a6b440247edf0d0ca87bb43
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-06-24 20:08:44 -0300

    ok

--- MiProyecto/src/components/ItemDetail/ItemDetail.jsx
+++ MiProyecto/src/components/ItemDetail/ItemDetail.jsx
@@ -1,44 +1,42 @@
-import { useState } from "react"
-import { useCartContext } from "../../context/CartContext"
-import ItemCount from "../ItemCount/ItemCount"
-import { Link } from "react-router-dom"
-import './ItemDetail.css';
+import React, { useState } from "react";
+import { useCartContext } from "../../context/CartContext";
+import ItemCount from "../ItemCount/ItemCount";
+import { Link } from "react-router-dom";
+import "./ItemDetail.css";
 
+export const ItemDetail = ({ title, imageUrl, price, description }) => {
+  const [isCant, setIsCant] = useState(false);
+  const { addToCart } = useCartContext();
 
-export const ItemDetail = ({ title, imageUrl, price, description  }) => {
-    const [isCant, setIsCant] = useState(false)
+  const onAdd = (cantidad) => {
+    addToCart({ title, imageUrl, price, description, cantidad });
+    setIsCant(true);
+  };
 
-    const { addToCart } = useCartContext()
-
-    const onAdd = (cantidad) => {
-        addToCart({ ...productos, cantidad })
-        setIsCant(true)
-    }
-
-
-
-    return (
+  return (
+    <>
+      <div className="item-detail-container">
+        <div className="item-image">
+          <img src={imageUrl} alt={title} style={{ maxWidth: "100%" }} />
+        </div>
+        <div className="item-info">
+          <h2>{title}</h2>
+          <p className="price">Precio: ${price}</p>
+          <p>{description}</p>
+        </div>
+      </div>
+      {!isCant ? (
+        <ItemCount onAdd={onAdd} />
+      ) : (
         <>
-            <div className="item-detail-container">
-                <div className="item-image">
-                    <img src={imageUrl} alt={title} />
-                </div>
-                <div className="item-info">
-                    <h2>{title}</h2>
-                    <p className="price">Precio: ${price}</p>
-                    <p>{description}</p>
-                </div>
-            </div>
-            {
-                !isCant ?
-                    <ItemCount onAdd={onAdd} />
-                    :
-                    <>
-                        <Link to={'/cart'} className="btn btn-outline-danger">Terminar compra</Link>
-                        <Link to={'/'} className="btn btn-outline-success">Seguir la compra</Link>
-                    </>
-            }
-
+          <Link to={"/cart"} className="btn btn-outline-danger">
+            Terminar compra
+          </Link>
+          <Link to={"/"} className="btn btn-outline-success">
+            Seguir la compra
+          </Link>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
