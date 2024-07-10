8c2d9293ab7c68bca99452d21248098c4db6e130
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-07-09 21:57:40 -0300

    x

--- MiProyecto/src/components/CartContainer/CartContainer.jsx
+++ MiProyecto/src/components/CartContainer/CartContainer.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import { useCartContext } from '../../context/CartContext'
+import { useCartContext } from "../../context/cartContext"
+
 
 export const CartContainer = () => {
-  const {cartList,vaciarCarrito }= useCartContext()
-  return (
-    <>
-      {cartList.map(prod=>{
-        <img src={prod.foto} alt='imagen'/>
-       
-      })}
-    </>
-  )
+    const {cartList,  vaciarCarrtio} = useCartContext
+
+    return (
+        <div>
+            {cartList.map(prod => (
+                <div className="w-50">
+                    <img className="w-25" src={prod.foto} alt="imagen"/>
+                    <label> Precio {prod.price} - Cantidad : {prod.cantidad}</label>
+                    <button > X </button>
+                </div>
+            ))}
+            <button onClick={vaciarCarrtio} className="btn btn-outline-danger">Vaciar Carrito</button>
+        </div>
+    )
 }
-
-
