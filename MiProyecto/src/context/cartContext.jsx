bbfb125be5dda9d34e94f6b12738379c081b42d5
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-07-09 21:52:22 -0300

    cartContext

--- MiProyecto/src/context/cartContext.jsx
+++ MiProyecto/src/context/cartContext.jsx
@@ -1,42 +1,26 @@
 import { createContext, useContext, useState } from "react"
 
-
-export const CartContext = createContext([])
+const CartContext = createContext([])
 
 export const useCartContext = () => useContext(CartContext)
 
 export const CartContextProvider = ({children})=> {
 
-  const [cartList, setCartList]=useState([])
+    const [cartList, setCartList] = useState([])
 
-  const addToCart = (newProduct) => {
-    const existingProductIndex = cartList.findIndex(item => item.title === newProduct.title);
-    if (existingProductIndex !== -1) {
-      // El producto ya existe en el carrito, actualiza la cantidad
-      const updatedCartList = cartList.map((item, index) => {
-        if (index === existingProductIndex) {
-          return { ...item, cantidad: item.cantidad + newProduct.cantidad };
-        }
-        return item;
-      });
-      setCartList(updatedCartList);
-    } else {
-      // El producto no está en el carrito, añádelo
-      setCartList([
-        ...cartList,
-        newProduct
-      ]);
-    }
-  };
+    const addToCart = (newProduct)=>{
   
-
+        setCartList([
+            ...cartList,
+            newProduct
+        ])
+    }
     const vaciarCarrtio = () => {
         setCartList([])
     }
 
     return (
        <CartContext.Provider value={{
-         
             cartList,
             addToCart,
             vaciarCarrtio
