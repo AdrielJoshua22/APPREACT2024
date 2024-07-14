9eb6592daff04e9772555b0a58a55ad3524a0b3f
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-07-14 13:15:09 -0300

    ok

--- MiProyecto/src/components/CartContainer/CartContainer.jsx
+++ MiProyecto/src/components/CartContainer/CartContainer.jsx
@@ -10,10 +10,10 @@ export const CartContainer = () => {
 
     return (
         <div>
-            {cartList.map((prod, index) => (
+            {cartList.map((producto, index) => (
                 <div key={index} className="w-50">
-                    <img className="w-25" src={prod.foto} alt="imagen del producto" />
-                    <label> Precio: {prod.price} - Cantidad: {prod.cantidad}</label>
+                    <img className="w-25" src={producto.foto} alt="imagen del producto" />
+                    <label> Precio: {producto.price} - Cantidad: {producto.cantidad}</label>
                     <button> X </button>
                 </div>
             ))}
