579c88a9596a28f29d3df878930a7490daf878d6
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-07-16 09:51:27 -0300

    k

--- MiProyecto/src/components/Contador/Contador.jsx
+++ MiProyecto/src/components/Contador/Contador.jsx
@@ -3,20 +3,20 @@ import { useCounter } from '../../hook/useCounter';
 import './contador.css';
 
 
-export const Contador = ({ initial = 1, stock = 5, onAdd }) => {
-    const { contador, incrementar, decrementar, resetear } = useCounter(initial, 1, stock);
+export const counter = ({ initial = 1, stock = 5, onAdd }) => {
+    const { counter, incrementar, decrementar, resetear } = useCounter(initial, 1, stock);
 
     const handleAddToCart = () => {
-        onAdd(contador);
+        onAdd(counter);
     };
 
     return (
-        <Container className='contador'>
-            <h3>{contador}</h3>
-            <button className='contador' onClick={() => decrementar()}>-1</button>
-            <button className='contador' onClick={() => resetear()}>Reset</button>
-            <button className='contador' onClick={() => incrementar()}>+1</button>
-            <button className='contador' onClick={handleAddToCart}>Agregar a carrito</button>
+        <Container className='counter'>
+            <h3>{counter}</h3>
+            <button className='counter' onClick={() => decrementar()}>-1</button>
+            <button className='counter' onClick={() => resetear()}>Reset</button>
+            <button className='counter' onClick={() => incrementar()}>+1</button>
+            <button className='counter' onClick={handleAddToCart}>Agregar a carrito</button>
         </Container>
     );
 };
