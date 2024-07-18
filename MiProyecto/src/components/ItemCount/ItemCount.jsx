c2fb5a9551537f5ccfcb7f43f83aace39d578f34
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-07-18 09:54:52 -0300

    buscando bug counter

--- MiProyecto/src/components/ItemCount/ItemCount.jsx
+++ MiProyecto/src/components/ItemCount/ItemCount.jsx
@@ -2,13 +2,13 @@ import { useCounter } from "../../hook/useCounter.jsx"
 
 const ItemCount = ({ inital=1, stock=5, onAdd }) => {
 
-    const { counter, handleSumar, handleRestar } = useCounter(inital, 1, stock);
+    const { counter, incrementar, decrementar } = useCounter(inital, 1, stock);
 
     return (
         <center>
-            <button onClick={handleSumar}> + 1</button> 
+            <button onClick={incrementar}> + 1</button> 
             <h3>{counter}</h3>
-            <button onClick={handleRestar}> - 1</button> 
+            <button onClick={decrementar}> - 1</button> 
             <button onClick={ ()=>{onAdd(counter)} }>Agregar al carrito</button>
         </center>
     )
