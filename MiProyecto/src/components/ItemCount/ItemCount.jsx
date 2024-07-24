aaa0b56ccb7b319f4cc35275bdd737d3a6f0d91d
Adriel Joshua <joshuaamengual9305@gmail.com>
2024-07-24 09:36:10 -0300

    ok

--- MiProyecto/src/components/ItemCount/ItemCount.jsx
+++ MiProyecto/src/components/ItemCount/ItemCount.jsx
@@ -1,18 +1,19 @@
-import { useCounter } from "../../hook/useCounter.jsx";
-import "./ItemCount.css";
-
 const ItemCount = ({ initial = 1, stock = 5, onAdd }) => {
     const { contador, incrementar, decrementar } = useCounter(initial, stock);
+
+    const handleAdd = () => {
+        const count = Number(contador); // Asegúrate de que contador es un número
+        onAdd(count);
+    };
 
     return (
         <center>
             <button onClick={incrementar}> + 1</button> 
             <h3>{contador} CONTADOR</h3>
             <button onClick={decrementar}> - 1</button> 
-            <button onClick={() => { onAdd(contador) }}>Agregar al carrito</button>
+            <button onClick={handleAdd}>Agregar al carrito</button>
         </center>
     );
 };
 
 export default ItemCount;
-
